46cea22b24c0758aab233739759dd22a2bf442e3
Steven Lara <steven.carcamo2019adoc@gmail.com>
2021-01-22 22:24:48 -0600

    I added CSS styles to the sidebar section

--- index.php
+++ index.php
@@ -35,17 +35,22 @@
             <!-- SIDEBAR -->
             <aside class="sidebar">
                 <div class="login block_aside">
+                    <h3>Entrar a la WEB</h3>
                     <form action="#" method="POST">
                         <label for="email">Email:</label>
                         <input type="email" name="email">
 
                         <label for="password">Contraseña:</label>
                         <input type="password" name="password">
+
+                        <input type="submit" value="Enviar">
                     </form>
 
-                    <a href="#">Mis pedidos</a>
-                    <a href="#">Gestionar pedidos</a>
-                    <a href="#">Gestionar categorias</a>
+                    <ul>
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                        <li><a href="#">Gestionar categorias</a></li>
+                    </ul>
                 </div>
             </aside>
 
