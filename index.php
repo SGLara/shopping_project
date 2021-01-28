2ac3c5d9e7e91dc9bd9b28726b6b472923ccf668
Steven Lara <steven.carcamo2019adoc@gmail.com>
2021-01-27 22:17:31 -0600

    I added CSS styles to main content and footer

--- index.php
+++ index.php
@@ -56,32 +56,47 @@
 
             <!-- MAIN -->
             <div class="main">
+            <h1>Productos destacados</h1>
                 <div class="product">
                     <img src="assets/img/camiseta.png" alt="camiseta.png">
                     <h2>Camiseta Azul Ancha</h2>
                     <p>$30 dólares</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
 
                 <div class="product">
                     <img src="assets/img/camiseta.png" alt="camiseta.png">
                     <h2>Camiseta Azul Ancha</h2>
                     <p>$30 dólares</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
 
                 <div class="product">
                     <img src="assets/img/camiseta.png" alt="camiseta.png">
                     <h2>Camiseta Azul Ancha</h2>
                     <p>$30 dólares</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
 
                 <div class="product">
                     <img src="assets/img/camiseta.png" alt="camiseta.png">
                     <h2>Camiseta Azul Ancha</h2>
                     <p>$30 dólares</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>$30 dólares</p>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>$30 dólares</p>
+                    <a href="#" class="button">Comprar</a>
                 </div>
             </div>
         </div>
