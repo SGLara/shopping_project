a4396c55da601ed88ba09aa5c683877d1f071d57
Steven Lara <steven.carcamo2019adoc@gmail.com>
2021-02-16 22:13:15 -0600

    Functions added to sidebar.php

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -24,10 +24,14 @@
          <?php endif; ?>
 
          <ul>
-             <li><a href="#">Mis pedidos</a></li>
-             <li><a href="#">Gestionar pedidos</a></li>
-             <li><a href="#">Gestionar categorias</a></li>
+             <?php if (isset($_SESSION['admin'])) : ?>
+                 <li><a href="#">Gestionar categorias</a></li>
+                 <li><a href="#">Gestionar productos</a></li>
+                 <li><a href="#">Gestionar pedidos</a></li>
+             <?php endif; ?>
+
              <?php if (isset($_SESSION['identity'])) : ?>
+                 <li><a href="#">Mis pedidos</a></li>
                  <li><a href="<?= base_url ?>user/logout">Cerrar Sesión</a></li>
              <?php endif; ?>
          </ul>
